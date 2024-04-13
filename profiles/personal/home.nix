22dbb57706f2123cca50b5c05d9b46c4bf8609da
Stefan Kugele <stefan.kugele@gmail.com>
2024-04-13 22:10:15 +0200

    firefox as default browser

--- profiles/personal/home.nix
+++ profiles/personal/home.nix
@@ -349,16 +349,16 @@
 		videos = "${config.home.homeDirectory}/desktop";
 	};
 
-	# xdg.mimeApps = {
-	# 	enable = true;
-		# defaultApplications = {
-		# 	"text/html" = "brave-browser.desktop";
-		# 	"x-scheme-handler/http" = "brave-browser.desktop";
-		# 	"x-scheme-handler/https" = "brave-browser.desktop";
-		# 	"x-scheme-handler/about" = "brave-browser.desktop";
-		# 	"x-scheme-handler/unknown" = "brave-browser.desktop";
-		# };
-  # };
+	xdg.mimeApps = {
+		enable = true;
+		defaultApplications = {
+			"text/html" = "firefox.desktop";
+			"x-scheme-handler/http" = "firefox.desktop";
+			"x-scheme-handler/https" = "firefox.desktop";
+			"x-scheme-handler/about" = "firefox.desktop";
+			"x-scheme-handler/unknown" = "firefox.desktop";
+		};
+  };
 
 	# programs.brave = {
 	# 	enable = true;
