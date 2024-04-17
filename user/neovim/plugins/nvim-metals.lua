1bbcf1def1639b520f61c87d4255714c95398c05
Stefan Kugele <stefan.kugele@gmail.com>
2024-04-17 23:09:05 +0200

    nvim-metals

--- user/neovim/plugins/nvim-metals.lua
+++ user/neovim/plugins/nvim-metals.lua
@@ -23,7 +23,7 @@ local metals_config = require("metals").bare_config()
 
     local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
     vim.api.nvim_create_autocmd("FileType", {
-      pattern = self.ft,
+      pattern = { "scala", "sbt", "java" },
       callback = function()
         require("metals").initialize_or_attach(metals_config)
       end,
