89ce0e19c8a312fa63e311553b4701f71fc54348
Stefan Kugele <stefan.kugele@gmail.com>
2024-04-17 23:24:25 +0200

    nvim-metals

--- user/neovim/plugins/nvim-metals.lua
+++ user/neovim/plugins/nvim-metals.lua
@@ -2,23 +2,6 @@
 local metals_config = require("metals").bare_config()
     metals_config.on_attach = function(client, bufnr)
       -- your on_attach function
-			vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-			vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-			vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-			vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-			vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-			vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-			vim.keymap.set('n', '<space>wl', function()
-				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-			end, opts)
-			vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-			vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, vim.tbl_deep_extend("error", opts, { desc = "rename" }))
-			vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-			vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-			vim.keymap.set('n', '<space>p', function()
-				vim.lsp.buf.format { async = true }
-			end, opts)
     end
 
     local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
