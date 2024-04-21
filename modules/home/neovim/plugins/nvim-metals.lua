a1e4630034eb2de59ec2a0d57dd66b5c7c0d8dd2
Stefan Kugele <stefan.kugele@gmail.com>
2024-04-21 11:40:03 +0200

    nvim-metals binaryPath

--- modules/home/neovim/plugins/nvim-metals.lua
+++ modules/home/neovim/plugins/nvim-metals.lua
@@ -1,10 +1,9 @@
 local metals_config = require("metals").bare_config()
 
-
 -- Example of settings
 metals_config.settings = {
-	metalsBinaryPath = '/nix/store/cc6h1m7rass3sdvc17f0zr5i8y76ymfa-metals-1.2.2/bin/metals',
-	-- metalsBinaryPath = vim.fn.system('nix path-info nixpkgs#metals'),
+	-- metalsBinaryPath = '/nix/store/cc6h1m7rass3sdvc17f0zr5i8y76ymfa-metals-1.2.2/bin/metals',
+	metalsBinaryPath = vim.trim(vim.fn.system('nix path-info nixpkgs#metals')) .. '/bin/metals',
 	showImplicitArguments = true,
 	excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
 }
@@ -21,7 +20,7 @@ end
 
 local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "scala", "sbt", "java" },
+	pattern = { "scala", "sbt" },
 	callback = function()
 		require("metals").initialize_or_attach(metals_config)
 	end,
