b12878fbf05ac738ded3882a0d35bc24f0af4023
Stefan Kugele <stefan.kugele@gmail.com>
2024-04-27 15:53:01 +0200

    static ip

--- profiles/personal/configuration.nix
+++ profiles/personal/configuration.nix
@@ -45,11 +45,16 @@
   networking = {
   	# hostName = "nixos";
   	# hostName = hostName;
+		interfaces = {
+			enp2s0.ipv4.addresses = [{
+				address = "192.168.178.20";
+				prefixLength = 24;
+			}];
+		};
   	hostName = systemSettings.hostname;
 		enableIPv6 = true;
 		networkmanager = {
 			enable = true;
-			dhcp = "dhcpcd";
 			# insertNameservers = [ "8.8.8.8" "8.8.4.4" ];
 			# unmanaged = [ "wlp3s0" ];
 		};
