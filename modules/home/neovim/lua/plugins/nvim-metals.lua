637a10c8b0d40bdeee29f6ac7dc04256f08fbb33
Stefan Kugele <stefan.kugele@gmail.com>
2024-05-07 16:34:12 +0200

    metals keymaps

--- modules/home/neovim/lua/plugins/nvim-metals.lua
+++ modules/home/neovim/lua/plugins/nvim-metals.lua
@@ -7,6 +7,7 @@ metals_config.settings = {
 	metalsBinaryPath = vim.trim(vim.fn.system('nix path-info nixpkgs#metals')) .. '/bin/metals',
 	showImplicitArguments = true,
 	excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+	autoImportBuild = "all",
 }
 
 metals_config.init_options.statusBarProvider = "off"
@@ -14,8 +15,26 @@ metals_config.init_options.statusBarProvider = "off"
 -- Example if you are using cmp how to make sure the correct capabilities for snippets are set
 metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-metals_config.on_attach = function(client, bufnr)
+metals_config.on_attach = function(ev)
 	-- your on_attach function
+		local opts = { buffer = ev.buf }
+		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+		vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+		vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+		vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+		vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+		vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+		vim.keymap.set('n', '<space>wl', function()
+			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		end, opts)
+		vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+		vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, vim.tbl_deep_extend("error", opts, { desc = "rename" }))
+		vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+		vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+		vim.keymap.set('n', '<space>p', function()
+			vim.lsp.buf.format { async = true }
+		end, opts)
 end
 
 
