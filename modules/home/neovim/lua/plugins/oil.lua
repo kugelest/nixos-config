4e80dfdd8ceefbd109536d0557edb6b67c944d37
Stefan Kugele <stefan.kugele@gmail.com>
2024-05-16 00:28:17 +0200

    oil git actions

--- modules/home/neovim/lua/plugins/oil.lua
+++ modules/home/neovim/lua/plugins/oil.lua
@@ -1,3 +1,15 @@
 require("oil").setup({
 	skip_confirm_for_simple_edits = true,
+	git = {
+		-- Return true to automatically git add/mv/rm files
+		add = function(path)
+			return false
+		end,
+		mv = function(src_path, dest_path)
+			return true
+		end,
+		rm = function(path)
+			return true
+		end,
+	}
 })
