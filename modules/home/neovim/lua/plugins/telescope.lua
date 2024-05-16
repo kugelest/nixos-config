59a2f5eb56a5a92b444434d361083e2c502e6314
Stefan Kugele <stefan.kugele@gmail.com>
2024-05-16 15:32:51 +0200

    telescope filename first

--- modules/home/neovim/lua/plugins/telescope.lua
+++ modules/home/neovim/lua/plugins/telescope.lua
@@ -1,13 +1,13 @@
 local custom_sorter = function(opts)
-  local score = require('telescope.sorters').get_fzy_sorter(opts)
-  return function(entry)
-    local basename = vim.fn.fnamemodify(entry.value, ':t')
-    local dirname = vim.fn.fnamemodify(entry.value, ':h')
-    return score({ ordinal = basename .. ' ' .. dirname, entry = entry })
-  end
+	local score = require('telescope.sorters').get_fzy_sorter(opts)
+	return function(entry)
+		local basename = vim.fn.fnamemodify(entry.value, ':t')
+		local dirname = vim.fn.fnamemodify(entry.value, ':h')
+		return score({ ordinal = basename .. ' ' .. dirname, entry = entry })
+	end
 end
 
-require('telescope').setup{
+require('telescope').setup {
 	defaults = {
 		mappings = {
 			i = {
@@ -15,15 +15,14 @@ require('telescope').setup{
 			}
 		},
 		path_display = {
+			"filename_first",
 			truncate = 1,
 		}
 	},
 	pickers = {
-    find_files = {
-      -- theme = "dropdown",
+		find_files = {
+			-- theme = "dropdown",
 			sorters = custom_sorter {}
-    }
-  },
+		}
+	},
 }
-
-
--- modules/home/neovim/lua/plugins/treesitter-context.lua
+++ modules/home/neovim/lua/plugins/treesitter-context.lua
@@ -1,5 +1,5 @@
 require('treesitter-context').setup {
-	enable = false,            -- Enable this plugin (Can be enabled/disabled later via commands)
+	enable = true,            -- Enable this plugin (Can be enabled/disabled later via commands)
 	max_lines = 1,            -- How many lines the window should span. Values <= 0 mean no limit.
 	min_window_height = 0,    -- Minimum editor window height to enable context. Values <= 0 mean no limit.
 	line_numbers = true,
@@ -9,5 +9,5 @@ require('treesitter-context').setup {
 	-- Separator between context and content. Should be a single character string, like '-'.
 	-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
 	separator = nil,
-	zindex = 1, -- The Z-index of the context window
+	zindex = 20, -- The Z-index of the context window
 }
