39c63746dae514482c2af0bf3b6223d89638f147
Stefan Kugele <stefan.kugele@gmail.com>
2024-06-08 14:19:33 +0200

    vcv-rack

--- profiles/personal/configuration.nix
+++ profiles/personal/configuration.nix
@@ -16,6 +16,7 @@
 	# services.xserver.videoDrivers = [ "modesetting" ];
 	hardware.opengl.enable = true;
 	hardware.opengl.driSupport = true;
+	hardware.opengl.driSupport32Bit = true;
 	# hardware.opengl.extraPackages = with pkgs; [
  #  	rocmPackages.clr.icd
  #  	amdvlk
